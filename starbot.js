c2f837c0602a06f3046484c159ae8edd03ed34c9
Miroware8 <grantgryczan@gmail.com>
2017-12-23 14:58:34 -0500

    What is wrong with me?

--- starbot.js
+++ starbot.js
@@ -41,7 +41,7 @@ var guildCreate = function(guild) {
 		allow: 268446736,
 		deny: 131072
 	}]).then(function(channel) {
-		data[guild.id] = [channel.id, "%E2%AD%90", 5, 16755763];
+		data.guilds[guild.id] = [channel.id, "%E2%AD%90", 5, 16755763];
 		save();
 	}).catch(function() {
 		warn(guild, "manage channels");
@@ -49,7 +49,7 @@ var guildCreate = function(guild) {
 };
 var guildDelete = function(guild) {
 	console.log("guildDelete " + guild.id);
-	delete data[guild.id];
+	delete data.guilds[guild.id];
 	save();
 }
 client.once("ready", function() {
@@ -60,19 +60,19 @@ client.once("ready", function() {
 	var guilds = Array.from(client.guilds.keys());
 	for(var i = 0; i < guilds.length; i++) {
 		var guild = client.guilds.get(guilds[i]);
-		if(data[guilds[i]]) {
-			if(!guild.channels.get(data[guilds[i]][0])) {
+		if(data.guilds[guilds[i]]) {
+			if(!guild.channels.get(data.guilds[guilds[i]][0])) {
 				guild.leave();
 			}
 		} else {
 			guildCreate(guild);
 		}
 	}
-	for(var i in data) {
+	for(var i in data.guilds) {
 		if(guilds.indexOf(i) == -1) {
 			guildDelete(i);
-		} else if(!data[i][1]) {
-			data[i][1] = "%E2%AD%90";
+		} else if(!data.guilds[i][1]) {
+			data.guilds[i][1] = "%E2%AD%90";
 		}
 	}
 	save();
@@ -80,7 +80,7 @@ client.once("ready", function() {
 client.on("guildCreate", guildCreate);
 client.on("guildDelete", guildDelete);
 client.on("channelDelete", function(channel) {
-	if(channel.id == data[channel.guild.id][0]) {
+	if(channel.id == data.guilds[channel.guild.id][0]) {
 		channel.guild.leave();
 	}
 });
@@ -92,9 +92,9 @@ var star = function(msg, callback) {
 	var embed = {
 		embed: {
 			timestamp: msg.createdAt.toISOString(),
-			color: data[msg.guild.id][3],
+			color: data.guilds[msg.guild.id][3],
 			footer: {
-				text: decodeURIComponent(data[msg.guild.id][1]) + " | " + msg.id
+				text: decodeURIComponent(data.guilds[msg.guild.id][1]) + " | " + msg.id
 			},
 			fields: [
 				{
@@ -127,13 +127,13 @@ var star = function(msg, callback) {
 			url: attachment.url
 		};
 	}
-	var starboard = msg.guild.channels.get(data[msg.guild.id][0]);
+	var starboard = msg.guild.channels.get(data.guilds[msg.guild.id][0]);
 	starboard.send(embed).then(callback).catch(function(err) {
 		warn(msg.guild, "read messages, send messages, " + (attachment ? "and/or embed links" : "embed links, and/or attach files") + ", in the " + starboard + " channel or otherwise");
 	});
 };
 client.on("messageReactionAdd", function(reaction) {
-	if(starred.indexOf(reaction.message.id) == -1 && data[reaction.message.guild.id] && reaction.message.channel.id != data[reaction.message.guild.id][0] && reaction.emoji.identifier == data[reaction.message.guild.id][1] && reaction.count >= data[reaction.message.guild.id][2]) {
+	if(starred.indexOf(reaction.message.id) == -1 && data.guilds[reaction.message.guild.id] && reaction.message.channel.id != data.guilds[reaction.message.guild.id][0] && reaction.emoji.identifier == data.guilds[reaction.message.guild.id][1] && reaction.count >= data.guilds[reaction.message.guild.id][2]) {
 		star(reaction.message);
 	}
 });
@@ -145,7 +145,7 @@ client.on("message", function(msg) {
 			var member = msg.guild.member(msg.author);
 			var perm = member.hasPermission(8);
 			var sendHelp = function() {
-				var help = msg.author + " You can add " + data[msg.guild.id][2] + " " + decodeURIComponent(data[msg.guild.id][1]) + " " + ((data[msg.guild.id][2] == 1) ? "reaction" : "reactions") + " to a message on this server to add it to the <#" + data[msg.guild.id][0] + "> channel.";
+				var help = msg.author + " You can add " + data.guilds[msg.guild.id][2] + " " + decodeURIComponent(data.guilds[msg.guild.id][1]) + " " + ((data.guilds[msg.guild.id][2] == 1) ? "reaction" : "reactions") + " to a message on this server to add it to the <#" + data.guilds[msg.guild.id][0] + "> channel.";
 				if(perm) {
 					help += "\nAs a member of this Discord server with administrative permission, you can enter \">⭐\" with, after it, a number to define how many reactions should get messages starred, an emoji (not custom) to define which emoji should be used to star messages, a hexademical color code to change the starred embed color, a channel tag to switch the starboard channel (not recommended), or a message ID to star that message manually.\nYou can also prevent me from scanning messages and accepting commands in a certain channel by adding me to its channel permissions and disabling my permission to read messages (except for in the starboard channel, which already has this disabled by default).";
 				}
@@ -157,18 +157,18 @@ client.on("message", function(msg) {
 			if(perm) {
 				content = content.replace(prefix, "").replace(/ /g, "");
 				if(content) {
-					var old1 = data[msg.guild.id][1];
-					data[msg.guild.id][1] = null;
+					var old1 = data.guilds[msg.guild.id][1];
+					data.guilds[msg.guild.id][1] = null;
 					msg.react(content).then(function(reaction) {
 						reaction.remove(client.user).then(function() {
-							data[msg.guild.id][1] = reaction.emoji.identifier;
+							data.guilds[msg.guild.id][1] = reaction.emoji.identifier;
 							save();
 							msg.channel.send(msg.author + " Members now have to react with the " + content + " emoji to get a message starred.").catch(function() {
 								warn(msg.guild, "send messages, in the " + msg.channel + " channel or otherwise");
 							});
 						});
 					}).catch(function() {
-						data[msg.guild.id][1] = old1;
+						data.guilds[msg.guild.id][1] = old1;
 						save();
 						msg.channel.messages.fetch(content).then(function(msg2) {
 							star(msg2, function() {
@@ -180,7 +180,7 @@ client.on("message", function(msg) {
 							if(channelTest.test(content)) {
 								var channel = content.replace(channelTest, "$1");
 								if(msg.guild.channels.get(channel)) {
-									data[msg.guild.id][0] = channel;
+									data.guilds[msg.guild.id][0] = channel;
 									save();
 									msg.channel.send(msg.author + " The starboard channel has been set to " + content + ".").catch(function() {
 										warn(msg.guild, "send messages, in the " + msg.channel + " channel or otherwise");
@@ -193,19 +193,19 @@ client.on("message", function(msg) {
 							} else {
 								var reactionCount = parseInt(content);
 								if(reactionCount) {
-									data[msg.guild.id][2] = Math.abs(reactionCount);
+									data.guilds[msg.guild.id][2] = Math.abs(reactionCount);
 									save();
-									msg.channel.send(msg.author + " Members now have to add " + data[msg.guild.id][2] + " " + ((data[msg.guild.id][2] == 1) ? "reaction" : "reactions") + " to get a message starred.").catch(function() {
+									msg.channel.send(msg.author + " Members now have to add " + data.guilds[msg.guild.id][2] + " " + ((data.guilds[msg.guild.id][2] == 1) ? "reaction" : "reactions") + " to get a message starred.").catch(function() {
 										warn(msg.guild, "send messages, in the " + msg.channel + " channel or otherwise");
 									});
 								} else if(colorTest.test(content)) {
 									var code = content.replace(colorTest, "$1$1$2$2$3$3$4");
-									data[msg.guild.id][3] = parseInt(code, 16);
+									data.guilds[msg.guild.id][3] = parseInt(code, 16);
 									save();
 									msg.channel.send("The starred embed color has been changed to `#" + code + "`.\n(The default starred embed color is `#ffac33`.)", {
 										embed: {
 											title: "#" + code,
-											color: data[msg.guild.id][3]
+											color: data.guilds[msg.guild.id][3]
 										}
 									}).catch(function() {
 										warn(msg.guild, "send messages or embed links, in the " + msg.channel + " channel or otherwise");
@@ -225,7 +225,7 @@ client.on("message", function(msg) {
 		}
 	}
 });
-client.login("MzY5MzEzNTM3MDczMzQ4NjA4.DMWtpA._s6cfRMlsEra6Csjgvw--656-hU");
+client.login(data.token);
 fs.watch(__filename, function(type) {
 	process.exit();
 });
