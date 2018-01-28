562900c525f972149d71ae0eac21f7658a8485bd
Miroware8 <grantgryczan@gmail.com>
2018-01-28 17:15:33 -0500

    Fixed everything?

--- pipe.js
+++ pipe.js
@@ -9,41 +9,45 @@ const s3 = new AWS.S3({
 	credentials: new AWS.Credentials(youKnow.s3),
 	sslEnabled: true
 });
-app.get("*", (req, res) => {
-	if(req.subdomain === "pipe") {
-		if(req.decodedPath === "/") {
-			res.redirect(`${req.protocol}://${req.get("Host").slice(5)}/pipe/`);
-		} else {
-			s3.getObject({
-				Bucket: "miroware-pipe",
-				Key: req.decodedPath.slice(1)
-			}, function(err, data) {
-				if(err) {
-					res.set("Content-Type", "text/plain").status(err.statusCode).send(`Error ${err.statusCode}: ${err.message}`);
-				} else {
-					res.set("Content-Type", data.ContentType).send(data.Body);
-				}
-			});
-		}
+app.use((req, res) => {
+	try {
+		req.decodedPath = decodeURIComponent(req.url);
+		req.next();
+	} catch(err) {
+		res.send("Error 400: Bad Request");
 	}
 });
-app.post("*", (req, res) => {
-	if(req.subdomain === "pipe") {
-		s3.putObject({
-			Body: req.body,
+app.get("*", (req, res) => {
+	if(req.decodedPath === "/") {
+		res.redirect("https://miroware.io/pipe/");
+	} else {
+		s3.getObject({
 			Bucket: "miroware-pipe",
-			Key: req.path.slice(1),
-			ContentType: mime.getType(req.path),
-			ServerSideEncryption: "AES256"
-		}, function(err) {
-			res.set("Content-Type", "text/plain");
+			Key: req.decodedPath.slice(1)
+		}, function(err, data) {
 			if(err) {
-				res.status(err.statusCode).send(`Error ${err.statusCode}: ${err.message}`);
+				res.set("Content-Type", "text/plain").status(err.statusCode).send(`Error ${err.statusCode}: ${err.message}`);
 			} else {
-				res.send(pathThingyThatDoesNotYetExist);
+				res.set("Content-Type", data.ContentType).send(data.Body);
 			}
 		});
 	}
+});
+app.post("*", (req, res) => {
+	s3.putObject({
+		Body: req.body,
+		Bucket: "miroware-pipe",
+		Key: req.decodedPath.slice(1),
+		ContentType: mime.getType(req.decodedPath),
+		ServerSideEncryption: "AES256"
+	}, function(err) {
+		res.set("Content-Type", "text/plain");
+		if(err) {
+			res.status(err.statusCode).send(`Error ${err.statusCode}: ${err.message}`);
+		} else {
+			res.send();
+		}
+	});
 });
 http.createServer(app).listen(8082);
 fs.watch(__filename, () => {
