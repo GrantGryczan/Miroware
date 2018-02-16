66aa71b7f10ceec84efa563f416ecd095012bc5a
Miroware8 <grantgryczan@gmail.com>
2018-02-15 23:48:17 -0500

    Removed unnecessary SSL

--- server.js
+++ server.js
@@ -5,25 +5,16 @@ const {html} = ServeCube;
 const request = require("request-promise-native");
 const mime = require("mime");
 const youKnow = require("./data/youknow.js");
-const options = {
+const cube = ServeCube.serve({
 	eval: v => {
 		return eval(v);
 	},
 	httpPort: 8081,
-	httpsPort: 8444,
 	subdomain: ["", "d"],
 	githubSecret: youKnow.github.secret,
 	githubPayloadURL: "/githubwebhook",
 	uncacheModified: process.argv[2] !== "production"
-};
-try {
-	options.tls = {
-		key: fs.readFileSync("/etc/letsencrypt/live/miroware.io/privkey.pem"),
-		cert: fs.readFileSync("/etc/letsencrypt/live/miroware.io/cert.pem"),
-		ca: fs.readFileSync("/etc/letsencrypt/live/miroware.io/chain.pem")
-	};
-} catch(err) {}
-const cube = ServeCube.serve(options);
+});
 const {load} = cube;
 const stdin = process.openStdin();
 stdin.on("data", function(input) {
