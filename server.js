d5a49e06da8af1038853aff21ef19fc0a8b9a5b1
Miroware8 <grantgryczan@gmail.com>
2018-02-21 19:46:53 -0500

    ServeCube 1.6.0

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const ServeCube = require("servecube");
 const {html} = ServeCube;
 const request = require("request-promise-native");
 const mime = require("mime");
+const session = require("express-session");
+//const Store = require("")(session);
 const youKnow = require("./data/youknow.js");
 const cube = ServeCube.serve({
 	eval: v => {
@@ -15,7 +17,20 @@ const cube = ServeCube.serve({
 	subdomain: ["", "d"],
 	githubSecret: youKnow.github.secret,
 	githubPayloadURL: "/githubwebhook",
-	uncacheModified: process.argv[2] !== "production"
+	uncacheModified: process.argv[2] !== "production"/*,/
+	middleware: [session({
+		name: "session",
+		secret: youKnow.sessionSecret,
+		resave: false,
+		saveUninitialized: false,
+		cookie: {
+			secure: true,
+			maxAge: 604800000
+		},
+		store: new Store({
+			
+		})
+	})]*/
 });
 const {load} = cube;
 const stdin = process.openStdin();
