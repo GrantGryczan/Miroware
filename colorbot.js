666db2127d9b8e62a8f2c7185727704e3ad364f4
Grant Gryczan <grantgryczan@gmail.com>
2018-03-06 21:12:59 -0500

    Test run

--- colorbot.js
+++ colorbot.js
@@ -75,7 +75,7 @@ client.once("ready", () => {
 });
 client.on("guildCreate", guildCreate);
 client.on("guildDelete", guildDelete);
-const star = (msg, callback) => {
+const colorEmbed = hexCode => {
 	if(data.guilds[msg.guild.id][0]) {
 		console.log(`star ${msg.guild.id} ${msg.channel.id} ${msg.id}`);
 		if(!starred.includes(msg.id)) {
@@ -129,27 +129,107 @@ const star = (msg, callback) => {
 };
 const prefix = /^> ?🖌 ?/;
 const colorTest = /^#?(?:([\da-f])([\da-f])([\da-f])|([\da-f]{6}))$/i;
+const properColorTest = /^#[\da-f]{6}$/;
 client.on("message", msg => {
 	if(msg.channel.type === "text" && !msg.system) {
 		let content = msg.content;
 		if(prefix.test(content)) {
-			const perm = msg.guild.member(msg.author).hasPermission(8);
-			if(perm) {
-				content = content.replace(prefix, "");
-				if(content && colorTest.test(content)) {
-					const code = content.replace(colorTest, "$1$1$2$2$3$3$4");
-					data.guilds[msg.guild.id][3] = parseInt(code, 16);
-					save();
-					msg.channel.send(`Your username color has been changed to \`#${code}\`.`, {
-						embed: {
-							title: `#${code}`,
-							color: data.guilds[msg.guild.id][3]
-						}
-					}).catch(() => {
-						permWarn(msg.guild, `send messages or embed links, in the ${msg.channel} channel or otherwise`);
-					});
+			content = content.replace(prefix, "");
+			if(content) {
+				const spaceIndex = content.indexOf(" ");
+				if(spaceIndex === -1) {
+					content = [content];
 				} else {
-					sendHelp(msg, perm);
+					content = [content.slice(0, spaceIndex), content.slice(spaceIndex+1)];
+				}
+				content[0] = content[0].toLowerCase();
+				if(content[0] === "set") {
+					if(colorTest.test(content[1])) {
+						content[1] = content[1].replace(colorTest, "#$1$1$2$2$3$3$4").toLowerCase();
+						const red = parseInt(content[1].slice(1, 3), 16);
+						const green = parseInt(content[1].slice(3, 5), 16);
+						const blue = parseInt(content[1].slice(5, 7), 16);
+						const addColorRole = () => {
+							const currentRole = guild.roles.find("name", content[1]);
+							if(currentRole) {
+								member.roles.add(currentRole).catch(err => {
+									permWarn(msg.guild, "manage roles");
+								});
+								msg.channel.send(msg.author + " Your color has been set.", {
+									embed: {
+										title: content[1],
+										color: parseInt(content[1].slice(1), 16)
+									}
+								}).catch(() => {
+									permWarn(msg.guild, `send messages or embed links, in the ${msg.channel} channel or otherwise`);
+								});
+							} else {
+								guild.roles.create({
+									data: {
+										name: content[1],
+										color: content[1],
+										permissions: 0
+									}
+								}).then(function(role) {
+									member.roles.add(role);
+									msg.channel.send(msg.author + " Your color has been set.");
+								}).catch(err => {
+									console.log(err.name, err.message);
+									if(err) {
+										permWarn(msg.guild, "manage roles");
+									} else {
+										const guildRoles = Array.from(guild.roles.values());
+										const colors = [];
+										for(let i of guildRoles) {
+											if(properColorTest.test(i.name)) {
+												const redDiff = parseInt(i.name.slice(1, 3), 16)-red;
+												const greenDiff = parseInt(i.name.slice(3, 5), 16)-green;
+												const blueDiff = parseInt(guildRoles[i].name.slice(5, 7), 16)-blue;
+												colors.push([i, redDiff*redDiff+greenDiff*greenDiff+blueDiff*blueDiff]);
+											}
+										}
+										colors = colors.sort((a, b) => a[1]-b[1]);
+										let roles = colors[0][0];
+										for(let i = 1; i < 10; i++) {
+											roles += " " + colors[i][0];
+										}
+										msg.channel.send(msg.author + " The maximum role limit has been reached and no more color roles can be created. If you want, you can choose a color that someone else is already using. Below are some similar colors I found to the one you entered.\n" + roles).catch(() => {
+											permWarn(msg.guild, `send messages, in the ${msg.channel} channel or otherwise`);
+										});
+									}
+								});
+							}
+						};
+						const roleArray = Array.from(member.roles.values());
+						const roleRemoved = false;
+						for(let i of roleArray) {
+							if(properColorTest.test(i.name)) {
+								if(Array.from(i.members.values()).length > 1) {
+									member.roles.remove(i).then(addColorRole);
+								} else {
+									i.delete().then(addColorRole);
+								}
+								roleRemoved = true;
+								break;
+							}
+						}
+						if(!roleRemoved) {
+							addColorRole();
+						}
+					} else {
+						msg.channel.send(`${msg.author} That's not a valid color code! If you don't know how color codes work, Google has a color picker built into the search page if you search "color picker".`);
+					}
+				} else {
+					const perm = msg.guild.member(msg.author).hasPermission(8);
+					if(perm) {
+						if(content[0] === "limit") {
+							
+						} else {
+							sendHelp(msg, perm);
+						}
+					} else {
+						sendHelp(msg, perm);
+					}
 				}
 			} else {
 				sendHelp(msg, perm);
