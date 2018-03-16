76fc2363d1a48d235385d1a4bcb069c400d28fec
Grant Gryczan <grantgryczan@gmail.com>
2018-03-16 16:03:15 -0400

    Embed original color on "maximum role limit" warnings

--- colorbot.js
+++ colorbot.js
@@ -204,7 +204,8 @@ client.on("message", async msg => {
 												}
 												msg.channel.send(`${msg.author} The maximum role limit has been reached and no more color roles can be created. If you want, you can choose a color that someone else is already using. Below are some similar colors I found to the one you entered.`, {
 													embed: {
-														description: colors.sort((a, b) => a[1]-b[1]).slice(0, 20).map(a => a[0]).join(" ")
+														description: colors.sort((a, b) => a[1]-b[1]).slice(0, 20).map(a => a[0]).join(" "),
+														color: parseInt(content[1].slice(1), 16)
 													}
 												}).catch(errEmbedLinks(msg));
 											}
