7be84b67d87a694923498036306c61aaa7ce4113
Grant Gryczan <grantgryczan@gmail.com>
2018-04-09 12:51:37 -0400

    MongoDB

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ const {serve, html} = require("servecube");
 const cookieParser = require("cookie-parser");
 const request = require("request-promise-native");
 const mime = require("mime");
+const {MongoClient} = require("mongodb");
 const session = require("express-session");
-//const Store = require("")(session);
+const MongoStore = require("connect-mongo")(session);
 const youKnow = require("./data/youknow.js");
 const production = process.argv[2] === "production";
 (async () => {
+	const client = await MongoClient.connect(youKnow.db.url, {
+		compression: "snappy"
+	});
+	const db = client.db("mspfa");
 	const cube = await serve({
 		eval: str => {
 			return eval(str);
@@ -23,19 +28,21 @@ const production = process.argv[2] === "production";
 		githubPayloadURL: "/githubwebhook",
 		githubSecret: youKnow.github.secret,
 		githubToken: youKnow.github.token,
-		middleware: [cookieParser()/*, session({
-			name: "session",
+		middleware: [cookieParser()session({
 			secret: youKnow.session.secret,
 			resave: false,
 			saveUninitialized: false,
+			name: "sess",
 			cookie: {
 				secure: true,
 				maxAge: 604800000
 			},
-			store: new Store({
-				
+			store: new MongoStore({
+				db,
+				collection: "sessions",
+				stringify: false
 			})
-		})*/]
+		})]
 	});
 	const {load} = cube;
 	process.openStdin().on("data", input => {
