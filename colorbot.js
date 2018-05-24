b0deabb2f31f9985922ce024fbafc7809a8a24cb
Grant Gryczan <grantgryczan@gmail.com>
2018-05-23 22:03:20 -0400

    Undo debug

--- colorbot.js
+++ colorbot.js
@@ -56,7 +56,6 @@ const errEmbedLinks = msg => () => {
 	permWarn(msg.guild, `send messages or embed links, in the ${msg.channel} channel or otherwise`);
 };
 const errManageRoles = msg => err => {
-	console.error(err);
 	permWarn(msg.guild, "manage roles, above mine or otherwise");
 };
 const sendHelp = (msg, perm) => {
