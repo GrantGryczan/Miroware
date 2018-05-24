2d0299a5483b4e6d0681308333293a8294bd3f18
Grant Gryczan <grantgryczan@gmail.com>
2018-05-24 02:34:42 -0400

    Future reference

--- www/login/index.js
+++ www/login/index.js
@@ -19,6 +19,12 @@
 					if(value === 0) {
 						Miro.request("PUT", "/users/@me", {}, {
 							
+						}).then(req => {
+							if(Math.floor(req.status/100) === 2) {
+								window.location.reload();
+							} else {
+								new Miro.dialog("Error", req.statusText, ["Okay"]);
+							}
 						});
 					} else {
 						Miro.logOut();
