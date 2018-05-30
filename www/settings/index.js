ebf9917cb8063e648979b54cc98a35f31f0214b8
Grant Gryczan <grantgryczan@gmail.com>
2018-05-29 23:04:09 -0400

    ...

--- www/settings/index.js
+++ www/settings/index.js
@@ -7,12 +7,14 @@
 		this.form[_input].select();
 	};
 	const closeField = function() {
-		this.form[_input].disabled = false;
-		this.form[_prevValue] = this.value;
-		this.form[_input].select();
+		this.form[_input].disabled = true;
+		this.value = this.form[_prevValue];
+	};
+	const saveField = function() {
+		// TODO
 	};
 	for(const v of document.querySelectorAll(".field")) {
-		v[_input] = v.previousSibling.querySelector("input");
+		v[_input] = v.querySelector("input");
 		v.querySelector(".editfield").addEventListener("click", editField);
 		v.querySelector(".closefield").addEventListener("click", closeField);
 		v.querySelector(".savefield").addEventListener("click", saveField);
