68b7d7bc3e80137aafa5d7112e8aaf5227ca477e
Grant Gryczan <grantgryczan@gmail.com>
2018-05-30 19:56:14 -0400

    Opposite of what I should have been doing

--- www/settings/index.js
+++ www/settings/index.js
@@ -10,14 +10,12 @@
 	savePrevs();
 	const onInput = evt => {
 		for(const v of inputs) {
-			if(!v.checkValidity() || v[_prev] !== (v.type === "checkbox" ? v.checked : v.value)) {
-				console.log(v.checkValidity(), v[_prev], v.type === "checkbox" ? v.checked : v.value);
-				settings.disabled = true;
+			if(v.checkValidity() && v[_prev] !== (v.type === "checkbox" ? v.checked : v.value)) {
+				settings.disabled = false;
 				return;
 			}
 		}
-		console.log(1);
-		settings.disabled = false;
+		settings.disabled = true;
 	};
 	settings.addEventListener("input", onInput);
 	settings.addEventListener("change", onInput);
