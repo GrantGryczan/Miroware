3e8f822ec030d8b53e113bccb53938d6e58418fa
Grant Gryczan <grantgryczan@gmail.com>
2018-05-29 22:26:48 -0400

    Forgot to actually call the script

--- www/settings/index.js
+++ www/settings/index.js
@@ -4,11 +4,11 @@
 	const editField = function() {
 		this.previousSibling.classList.toggle("mdc-text-field--disabled");
 		this[_input].disabled = !this[_input].disabled;
-		this[_icon].textContent = (this[_icon].textContent === "edit" ? "save" : "edit")
+		this[_icon].textContent = (this[_icon].textContent === "edit" ? "save" : "edit");
 	};
 	for(const v of document.querySelectorAll(".editfield")) {
 		v[_input] = v.previousSibling.querySelector("input");
 		v[_icon] = v.querySelector(".mdc-fab__icon");
 		v.addEventListener("click", editField);
 	}
-});
+})();
