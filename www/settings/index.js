6d58a579004083cd6850f69d8b27905759e12b77
Grant Gryczan <grantgryczan@gmail.com>
2018-05-29 23:05:22 -0400

    Show appropriate icons

--- www/settings/index.js
+++ www/settings/index.js
@@ -2,16 +2,22 @@
 	const _input = Symbol("input");
 	const _prevValue = Symbol("prevValue");
 	const editField = function() {
+		this.parentNode.classList.add("hidden");
+		this.parentNode.nextSibling.classList.remove("hidden");
 		this.form[_input].disabled = false;
 		this.form[_prevValue] = this.value;
 		this.form[_input].select();
 	};
 	const closeField = function() {
+		this.parentNode.classList.add("hidden");
+		this.parentNode.previousSibling.classList.remove("hidden");
 		this.form[_input].disabled = true;
 		this.value = this.form[_prevValue];
 	};
 	const saveField = function() {
 		// TODO
+		this.parentNode.classList.add("hidden");
+		this.parentNode.previousSibling.classList.remove("hidden");
 	};
 	for(const v of document.querySelectorAll(".field")) {
 		v[_input] = v.querySelector("input");
