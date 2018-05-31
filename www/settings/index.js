05d51d1278d79327224f1e6224571a3b5a32b372
Grant Gryczan <grantgryczan@gmail.com>
2018-05-30 20:16:24 -0400

    Disable button, not form

--- www/settings/index.js
+++ www/settings/index.js
@@ -1,6 +1,7 @@
 (() => {
-	const settings = document.querySelector("#settings");
-	const inputs = settings.querySelectorAll("input");
+	const form = document.querySelector("#settings");
+	const inputs = form.querySelectorAll("input");
+	const submit = document.querySelector("#save");
 	const _prev = Symbol("prev");
 	const savePrevs = () => {
 		for(const v of inputs) {
@@ -10,17 +11,16 @@
 	savePrevs();
 	const onInput = evt => {
 		for(const v of inputs) {
-			console.log(v.checkValidity(), v[_prev], (v.type === "checkbox" ? v.checked : v.value));
 			if(v.checkValidity() && v[_prev] !== (v.type === "checkbox" ? v.checked : v.value)) {
-				settings.disabled = false;
+				submit.disabled = false;
 				return;
 			}
 		}
-		settings.disabled = true;
+		submit.disabled = true;
 	};
-	settings.addEventListener("input", onInput);
-	settings.addEventListener("change", onInput);
-	settings.addEventListener("submit", evt => {
+	form.addEventListener("input", onInput);
+	form.addEventListener("change", onInput);
+	form.addEventListener("submit", evt => {
 		evt.preventDefault();
 		savePrevs();
 	});
