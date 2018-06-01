09953e94f55c12b455cef958c8aca25f0e00a4da
Grant Gryczan <grantgryczan@gmail.com>
2018-06-01 00:47:41 -0400

    Disabled checkbox CSS

--- www/js/script.js
+++ www/js/script.js
@@ -82,7 +82,7 @@
 	};
 	const checkmark = html`
 		<svg class="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-			<path class="mdc-checkbox__checkmark-path" fill="none" stroke="white" d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
+			<path class="mdc-checkbox__checkmark-path" d="M1.73,12.91 8.1,19.28 22.79,4.59"/>
 		</svg>
 	`;
 	Miro.block = state => {
