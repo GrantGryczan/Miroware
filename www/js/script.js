81662bdd198d4fde333be3a3ef50207d31be9bf7
Grant Gryczan <grantgryczan@gmail.com>
2018-06-08 00:10:25 -0400

    Bind `location.reload` to `location`

--- www/js/script.js
+++ www/js/script.js
@@ -433,7 +433,7 @@
 			resolveAuth = resolve;
 		});
 	};
-	Miro.logOut = () => Miro.request("DELETE", "/token").then(Miro.response(location.reload));
+	Miro.logOut = () => Miro.request("DELETE", "/token").then(Miro.response(location.reload.bind(location)));
 	if(Miro.in = JSON.parse(document.querySelector("meta[name=\"in\"]").getAttribute("content"))) {
 		document.querySelector("#logOut").addEventListener("click", () => {
 			new Miro.dialog("Log out", "Are you sure you want to log out?", ["Yes", "No"]).then(value => {
