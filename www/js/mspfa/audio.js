d83fc0f88d6ce45da567ca0d058b6f8854f4ef77
Grant Gryczan <grantgryczan@gmail.com>
2018-07-22 01:44:10 -0400

    Modern code

--- www/js/mspfa/audio.js
+++ www/js/mspfa/audio.js
@@ -1,45 +1,45 @@
-(function() {
-	var ruleData = [];
-	var ready = false;
-	var slide = function() {
-		for(var i = 0; i < ruleData.length; i++) {
-			if(p >= ruleData[i][0] && p <= ruleData[i][1]) {
-				if(ruleData[i][2].paused) {
-					ruleData[i][2].play();
+(() => {
+	const rules = [];
+	let ready = false;
+	const slide = () => {
+		for(const rule of rules) {
+			if(p >= rule[0] && p <= rule[1]) {
+				if(rule[2].paused) {
+					rule[2].play();
 				}
-			} else if(!ruleData[i][2].paused) {
-				ruleData[i][2].pause();
-				ruleData[i][2].currentTime = 0;
+			} else if(!rule[2].paused) {
+				rule[2].pause();
+				rule[2].currentTime = 0;
 			}
 		}
 	};
-	var loaded = 0;
-	var load = function() {
-		if(++loaded === ruleData.length) {
+	let loaded = 0;
+	const load = () => {
+		if(++loaded === rules.length) {
 			ready = true;
 			slide();
 		}
 	};
-	var error = function(evt) {
-		var message = document.createElement("span");
+	const error = evt => {
+		const message = document.createElement("span");
 		message.appendChild(document.createTextNode("Audio failed to load:"));
 		message.appendChild(document.createElement("br"));
-		var audioLink = document.createElement("a");
+		const audioLink = document.createElement("a");
 		audioLink.textContent = audioLink.href = evt.target.src;
 		audioLink.target = "_blank";
 		message.appendChild(audioLink);
 		MSPFA.dialog("Error", message, ["Okay"]);
 		load();
 	};
-	var ruleTest = /@mspfa audio (\d+) (\d+) (.+?)(?:;|\n|$)/g;
-	var ruleMatch;
+	const ruleTest = /@mspfa audio (\d+) (\d+) (.+?)(?:;|\n|$)/g;
+	let ruleMatch;
 	while(ruleMatch = ruleTest.exec(MSPFA.story.y)) {
-		var audio = new Audio(ruleMatch[3]);
+		const audio = new Audio(ruleMatch[3]);
 		audio.addEventListener("load", load);
 		audio.addEventListener("error", error);
-		ruleData.push([parseInt(ruleMatch[1]) || 1, parseInt(ruleMatch[2]) || Infinity, audio]);
+		rules.push([parseInt(ruleMatch[1]) || 1, parseInt(ruleMatch[2]) || Infinity, audio]);
 	}
-	MSPFA.slide.push(function(page) {
+	MSPFA.slide.push(page => {
 		p = page;
 		slide();
 	});
