7334838b3058b6b294dd0e6aa9d612278d90f6b7
Grant Gryczan <grantgryczan@gmail.com>
2018-08-01 08:58:14 -0400

    Support CDG domain

--- proxy.js
+++ proxy.js
@@ -12,6 +12,7 @@ const redbird = require("redbird")({
 	}
 });
 redbird.register("pipe.miroware.io", "http://localhost:8082");
+redbird.register("comedy-dot.gold", "http://localhost:8082");
 redbird.addResolver(() => "http://localhost:8081");
 fs.watch(__filename, () => {
 	setTimeout(() => {
