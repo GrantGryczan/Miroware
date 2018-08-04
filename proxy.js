e3ecfe048862da0ca1fd64bd05aaad14a95d567f
Grant Gryczan <grantgryczan@gmail.com>
2018-08-04 16:07:20 -0400

    Update proxy

--- proxy.js
+++ proxy.js
@@ -1,20 +1,41 @@
 "use strict";
 console.log("< Proxy >");
 const fs = require("fs");
-const redbird = require("redbird")({
-	port: 8080,
-	secure: true,
-	ssl: {
-		port: 8443,
-		key: "/etc/letsencrypt/live/miroware.io/privkey.pem",
-		cert: "/etc/letsencrypt/live/miroware.io/cert.pem",
-		ca: "/etc/letsencrypt/live/miroware.io/chain.pem"
-	},
-	bunyan: false
+const http = require("http");
+const https = require("https");
+const tls = require("tls");
+const httpProxy = require("http-proxy");
+const ioContext = tls.createSecureContext({
+	key: fs.readFileSync("/etc/letsencrypt/live/miroware.io/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/miroware.io/cert.pem"),
+	ca: fs.readFileSync("/etc/letsencrypt/live/miroware.io/chain.pem")
 });
-const domainCDGTest = /(?:.*\.)?comedy-dot\.gold$/;
-redbird.register("pipe.miroware.io", "http://localhost:8082");
-redbird.addResolver(host => domainCDGTest.test(host) ? "http://localhost:8180" : "http://localhost:8081");
+const ggContext = tls.createSecureContext({
+	key: fs.readFileSync("/etc/letsencrypt/live/miro.gg/privkey.pem"),
+	cert: fs.readFileSync("/etc/letsencrypt/live/miro.gg/cert.pem"),
+	ca: fs.readFileSync("/etc/letsencrypt/live/miro.gg/chain.pem")
+});
+const proxy = httpProxy.createProxyServer();
+const listener = (req, res) => {
+	let target = "http://localhost:8081"
+	if(req.headers.host) {
+		if(req.headers.host.endsWith(".gold")) {
+			target = "http://localhost:8180";
+		} else if(req.headers.host.startsWith("pipe.")) {
+			target = "http://localhost:8082";
+		}
+	}
+	proxy.web(req, res, {
+		target
+	});
+};
+http.createServer(listener).listen(8080);
+https.createServer({
+	SNICallback: (domain, callback) => {
+		callback(null, domain.endsWith(".gg") ? ggContext : ioContext);
+	},
+	...ioContext
+}, listener).listen(8443);
 fs.watch(__filename, () => {
 	setTimeout(() => {
 		process.exit();
