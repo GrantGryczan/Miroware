da3230a7c6dc642facee2638ad7c9813be8c5d39
Grant Gryczan <grantgryczan@gmail.com>
2018-08-19 09:22:08 -0400

    ~

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ screen -d -m ./forever.sh ./proxy.js
 screen -d -m ./forever.sh ./server.js production
 screen -d -m ./forever.sh ./pipe.js
 screen -d -m ./forever.sh ./concat.js
+screen -d -m ./forever.sh ./mirobot.js
 screen -d -m ./forever.sh ./starbot.js
 screen -d -m ./forever.sh ./colorbot.js
 screen -d -m ./forever.sh ./nohmyt.js
