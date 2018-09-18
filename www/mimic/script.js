b690bae41f4eb83b00ed52b7f5a20202fa4d49da
Grant Gryczan <grantgryczan@gmail.com>
2018-09-17 21:26:45 -0400

    Version 1

--- www/mimic/script.js
+++ www/mimic/script.js
@@ -1,4 +1,26 @@
 "use strict";
-(() => {
-	
-})();
+const input = document.querySelector("#input");
+const output = document.querySelector("#output");
+output.addEventListener("focus", output.select.bind(output));
+const splitTest = /([^\w])/;
+const truthy = item => item;
+const start = () => {
+	const values = [...input.value.split(splitTest).filter(truthy), false];
+	const dictAfter = {};
+	for(let i = 0; i < values.length - 1; i++) {
+		const value = values[i];
+		if(!dictAfter[value]) {
+			dictAfter[value] = [];
+		}
+		dictAfter[value].push(values[i + 1]);
+	}
+	let string = "";
+	let phrase = values[0];
+	do {
+		string += phrase;
+		const after = dictAfter[phrase];
+		phrase = after[Math.floor(Math.random() * after.length)];
+	} while(phrase);
+	output.value = string;
+};
+document.querySelector("#start").addEventListener("click", start);
