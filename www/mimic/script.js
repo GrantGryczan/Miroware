65c75d8042bcf0d39f4598040f5410d54edc2c2e
Grant Gryczan <grantgryczan@gmail.com>
2018-09-17 22:04:03 -0400

    Version 2

--- www/mimic/script.js
+++ www/mimic/script.js
@@ -4,10 +4,17 @@ const output = document.querySelector("#output");
 output.addEventListener("focus", output.select.bind(output));
 const splitTest = /([^\w])/;
 const truthy = item => item;
+const randomItem = array => array[Math.floor(Math.random() * array.length)];
 const start = () => {
-	const values = [...input.value.split(splitTest).filter(truthy), false];
+	const values = input.value.split(splitTest).filter(truthy);
+	if(values[0] !== "\n") {
+		values.unshift("\n");
+	}
+	if(values[values.length - 1] !== "\n") {
+		values.push("\n");
+	}
 	const dictAfter = {};
-	for(let i = 0; i < values.length - 1; i++) {
+	for(let i = 0; i < values.length; i++) {
 		const value = values[i];
 		if(!dictAfter[value]) {
 			dictAfter[value] = [];
@@ -15,12 +22,15 @@ const start = () => {
 		dictAfter[value].push(values[i + 1]);
 	}
 	let string = "";
-	let phrase = values[0];
-	do {
-		string += phrase;
-		const after = dictAfter[phrase];
-		phrase = after[Math.floor(Math.random() * after.length)];
-	} while(phrase);
+	let phrase = "\n";
+	while(true) {
+		phrase = randomItem(dictAfter[phrase]);
+		if(phrase) {
+			string += phrase;
+		} else {
+			break;
+		}
+	}
 	output.value = string;
 };
 document.querySelector("#start").addEventListener("click", start);
