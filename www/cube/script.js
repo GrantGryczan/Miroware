37d0939925556b45cc5d2085a2fff5157bd8b34b
Grant Gryczan <grantgryczan@gmail.com>
2018-09-30 18:00:56 -0400

    Make variables global

--- www/cube/script.js
+++ www/cube/script.js
@@ -1,46 +1,46 @@
 "use strict";
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer({
+window.scene = new THREE.Scene();
+window.renderer = new THREE.WebGLRenderer({
 	alpha: true,
 	preserveDrawingBuffer: true
 });
 renderer.setSize(512, 512);
-const camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1024);
+window.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 1024);
 //scene.background = new THREE.Color(0x202020);
 camera.position.set(20, 0, 0);
 camera.lookAt(scene.position);
-const light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
+window.light = new THREE.HemisphereLight(0xffffff, 0x000000, 1);
 scene.add(light);
-const all = new THREE.Group();
+window.all = new THREE.Group();
 all.rotation.set(0.88888888, 0.88888888, 0.88888888);
-const group = new THREE.Group();
-const geometry = new THREE.BoxGeometry(8, 8, 8);
-const material = new THREE.MeshLambertMaterial({
+window.group = new THREE.Group();
+window.geometry = new THREE.BoxGeometry(8, 8, 8);
+window.material = new THREE.MeshLambertMaterial({
 	color: 0xff0000
 });
-const cube = new THREE.Mesh(geometry, material);
+window.cube = new THREE.Mesh(geometry, material);
 group.add(cube);
 all.add(group);
 scene.add(all);
 /*
-const ribbon = new THREE.MeshLambertMaterial({
+window.ribbon = new THREE.MeshLambertMaterial({
 	color: 0x00ff00
 });
-const ribbon1 = new THREE.Mesh(new THREE.BoxGeometry(8.25, 8.25, 2), ribbon);
-const ribbon2 = new THREE.Mesh(new THREE.BoxGeometry(8.25, 2, 8.25), ribbon);
+window.ribbon1 = new THREE.Mesh(new THREE.BoxGeometry(8.25, 8.25, 2), ribbon);
+window.ribbon2 = new THREE.Mesh(new THREE.BoxGeometry(8.25, 2, 8.25), ribbon);
 cube.add(ribbon1);
 cube.add(ribbon2);
 */
 let frame = 0;
-const render = index => {
+window.render = index => {
 	group.rotation.x = 2 * Math.PI * index / 256;
 	renderer.render(scene, camera);
 };
-const animate = () => {
+window.animate = () => {
 	render(frame = (frame + 1) % 64);
 	setTimeout(animate, 40);
 };
-const download = max => {
+window.download = max => {
 	max = typeof max === "number" ? max : 64;
 	const link = document.createElement("a");
 	for(let i = 0; i < max; i++) {
