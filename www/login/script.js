300ec3f01202e05868fa6191f80da9d83d6835cb
Grant Gryczan <grantgryczan@gmail.com>
2018-11-28 09:28:34 -0500

    ~

--- www/login/script.js
+++ www/login/script.js
@@ -77,7 +77,7 @@ loginForm.addEventListener("submit", evt => {
 			}
 		});
 		signupDialog.form.elements.email.value = loginForm.elements.email.value;
-		signupDialog.form.elements.email.querySelector("label").classList.add("mdc-floating-label--float-above");
+		signupDialog.form.elements.email.parentNode.querySelector("label").classList.add("mdc-floating-label--float-above");
 		setTimeout(signupDialog.form.elements.name.focus.bind(signupDialog.form.elements.name));
 	} else {
 		Miro.auth("Login", "Choose a login method.", logIn, dialogCallback).then(loggedIn);
