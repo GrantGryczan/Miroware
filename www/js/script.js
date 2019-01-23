3cf3ecc34427762bc26dc84aab355eaa6393ab50
Grant Gryczan <grantgryczan@gmail.com>
2019-01-23 18:52:48 -0500

    Use strict

--- www/js/script.js
+++ www/js/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const Miro = window.Miro = {};
 Miro.magic = {};
 Miro.magic.magic = Miro.magic;
--- www/wow/script.js
+++ www/wow/script.js
@@ -1,3 +1,4 @@
+"use strict";
 const magical = document.body.querySelector("#magical");
 let loops = 0;
 setInterval(() => {
