13152ec109c1fa1dea99fde7d760db416abb3062
Grant Gryczan <grantgryczan@gmail.com>
2019-02-01 11:02:47 -0500

    Encode opening parentheses in thumbnail background URL

--- www/users/{user}/pipe/script.js
+++ www/users/{user}/pipe/script.js
@@ -59,6 +59,7 @@ const getName = name => path ? name.slice(path.length + 1) : name;
 const applyPath = name => (path ? `${path}/` : "") + name;
 const encodedSlashes = /%2F/g;
 const encodeForPipe = name => encodeURIComponent(name).replace(encodedSlashes, "/");
+const openingParentheses = /\(/g;
 const closingParentheses = /\)/g;
 const pipe = [];
 const cachedPaths = [];
@@ -144,7 +145,7 @@ const PipeItem = class PipeItem {
 	}
 	updateThumbnail() {
 		if (this.type.startsWith("image/")) {
-			this.thumbnailElement.style.backgroundImage = `url(${this.url.replace(closingParentheses, "%29")})`;
+			this.thumbnailElement.style.backgroundImage = `url(${this.url.replace(openingParentheses, "%28").replace(closingParentheses, "%29")})`;
 		} else {
 			this.thumbnailElement.style.backgroundImage = "";
 			this.thumbnailElement.textContent = this.iconElement.textContent;
