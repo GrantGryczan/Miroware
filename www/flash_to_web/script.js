03991f5cbf21d00872104c85b1ba75bbb987da7c
Grant Gryczan <grantgryczan@gmail.com>
2019-02-21 22:18:17 -0500

    Remove array placeholders, implement floats

--- www/flash_to_web/script.js
+++ www/flash_to_web/script.js
@@ -2,23 +2,23 @@ const form = document.body.querySelector("#form");
 const panel = form.querySelector("#panel");
 let data;
 const alignToByte = () => {
-	if(bit > 0) {
+	if (bit > 0) {
 		byte++;
 		bit = 0;
 	}
 };
 const SWF = {
 	SI8: () => { // Signed 8-bit integer value
-		const byte = SWF.UI8();
-		return byte >> 7 ? byte - (1 << 8) : byte;
+		const value = SWF.UI8();
+		return value >>> 7 ? value - (1 << 8) : value;
 	},
 	SI16: () => { // Signed 16-bit integer value
-		const byte = SWF.UI16();
-		return byte >> 15 ? byte - (1 << 16) : byte;
+		const value = SWF.UI16();
+		return value >>> 15 ? value - (1 << 16) : value;
 	},
 	SI32: () => { // Signed 32-bit integer value
-		const byte = SWF.UI32();
-		return byte >> 31 ? byte - (1 << 32) : byte;
+		const value = SWF.UI32();
+		return value >>> 31 ? value - (1 << 32) : value;
 	},
 	SI8Array: n => { // Signed 8-bit array
 		
@@ -34,21 +34,13 @@ const SWF = {
 		alignToByte();
 		return data.bytes[data.byte++] | data.bytes[data.byte++] << 8;
 	},
+	UI24: () => { // Unsigned 24-bit integer value
+		alignToByte();
+		return (data.bytes[data.byte++] | data.bytes[data.byte++] << 8) | data.bytes[data.byte++] << 16;
+	},
 	UI32: () => { // Unsigned 32-bit integer value
 		alignToByte();
 		return ((data.bytes[data.byte++] | data.bytes[data.byte++] << 8) | data.bytes[data.byte++] << 16) | data.bytes[data.byte++] << 24;
-	},
-	UI8Array: n => { // Unsigned 8-bit array
-		
-	},
-	UI16Array: n => { // Unsigned 16-bit array
-		
-	},
-	UI24Array: n => { // Unsigned 24-bit array
-		
-	},
-	UI32Array: n => { // Unsigned 32-bit array
-		
 	},
 	FIXED: () => { // 32-bit 16.16 fixed-point number
 		alignToByte();
@@ -58,15 +50,21 @@ const SWF = {
 		alignToByte();
 		
 	},
-	FLOAT16: () => { // Half-prevision (16-bit) floating-point number
-		alignToByte();
-		
+	FLOAT16: () => { // Half-precision (16-bit) floating-point number
+		const value = SWF.UI16();
+		const sign = value >>> 15 ? -1 : 1;
+		const exponent = ((value << 17) >>> 27) - 15;
+		const mantissa = (value << 22) >>> 22;
+		return exponent === 16 ? (mantissa ? NaN : sign * Infinity) : sign * 2 ** exponent * (2 ** (Math.log(mantissa) / Math.log(2) - 10) + (exponent ? 1 : 0));
 	},
-	FLOAT: () => { // Half-prevision (32-bit) IEEE Standard 754 compatible
-		alignToByte();
-		
+	FLOAT: () => { // Half-precision (32-bit) IEEE Standard 754 compatible
+		const value = SWF.UI32();
+		const sign = value >>> 31 ? -1 : 1;
+		const exponent = ((value << 1) >>> 24) - 127;
+		const mantissa = (value << 9) >>> 9;
+		return exponent === 128 ? (mantissa ? NaN : sign * Infinity) : sign * 2 ** exponent * (2 ** (Math.log(mantissa) / Math.log(2) - 23) + (exponent ? 1 : 0));
 	},
-	DOUBLE: () => { // Half-prevision (64-bit) IEEE Standard 754 compatible
+	DOUBLE: () => { // Half-precision (64-bit) IEEE Standard 754 compatible
 		alignToByte();
 		
 	},
@@ -99,19 +97,19 @@ fileInput.addEventListener("change", () => {
 		};
 		try {
 			data.file.Signature = String.fromCharCode(...data.array.slice(0, 3));
-			if(data.file.Signature.slice(1) !== "WS") {
+			if (data.file.Signature.slice(1) !== "WS") {
 				throw new Error("Invalid SWF signature");
 			}
 			const compression = data.file.Signature[0];
-			if(compression === "F") {
+			if (compression === "F") {
 				data.bytes = data.array.slice(8);
-			} else if(compression === "C") {
+			} else if (compression === "C") {
 				data.bytes = pako.inflate(data.array.slice(8));
-			} else if(compression === "Z") {
+			} else if (compression === "Z") {
 				const result = LZMA.decompress(data.array.slice(8));
-				if(typeof result === "string") {
+				if (typeof result === "string") {
 					data.bytes = new Uint8Array(result.length);
-					for(let i = 0; i < result.length; i++) {
+					for (let i = 0; i < result.length; i++) {
 						data.bytes[i] = result[i].charCodeAt();
 					}
 				} else {
@@ -128,7 +126,7 @@ fileInput.addEventListener("change", () => {
 		} catch(err) {
 			new Miro.Dialog("Error", html`An error occurred while trying to read the SWF file.<br>$${err}`);
 		}
-		console.log(SWF, data);
+		console.log(data);
 		Miro.formState(form, true);
 		Miro.progress.close();
 	});
