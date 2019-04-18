f1b6d5d737337e450f18e8d82a8a8c0304d43afe
Grant Gryczan <grantgryczan@gmail.com>
2019-04-18 19:26:01 -0400

    Remove target indication for items dragged on the page

--- www/users/{user}/pipe/script.js
+++ www/users/{user}/pipe/script.js
@@ -1069,7 +1069,8 @@ if (Miro.data.isMe) {
 	});
 	let allowDrop = true;
 	document.addEventListener("dragstart", evt => {
-		allowDrop = false;
+		allowDrop = false; // This prevents things on the page from being dropped to the same page.
+		indicateTarget();
 	}, {
 		capture: true,
 		passive: true
