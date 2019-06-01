2b8594db0aef23f530c97fd5dbbc96b800c79a48
Grant Gryczan <grantgryczan@gmail.com>
2019-06-01 18:48:25 -0400

    Make role management permission let you manage Colorbot

--- colorbot.js
+++ colorbot.js
@@ -64,7 +64,7 @@ const sendHelp = (msg, perm) => {
 	const permOrNoGuild = perm || noGuild;
 	let help = `${noGuild ? "" : `${msg.author} `}You can use the following commands.${(noGuild || data.guilds[msg.guild.id][0] || perm) ? `\n\n\`>🎨color <hex color code>\`\nSet your color${permOrNoGuild ? ", if open color mode is enabled" : ""}.\n\n\`>🎨reset\`\nReset your color role${permOrNoGuild ? ", if open color mode is enabled" : ""}.` : ""}\n\n\`>🎨list\`\nList all role groups and their roles.\n\n\`>🎨add <role name>\`\nGive yourself a role.\n\n\`>🎨remove <role name>\`\nRemove a role from yourself.`;
 	if (permOrNoGuild) {
-		help += "\n\nWith administrative permission, you can use the following commands.\n\n`>🎨open`\nToggle open color mode. This is disabled by default.\n\n`>🎨create <group name>`\nCreate a role group.\n\n`>🎨group <group name> <role name>`\nAdd a role to a role group.\n\n`>🎨ungroup <role name>`\nRemove a role from its role group.\n\n`>🎨limit <group name> <number>`\nLimit how many roles each user can have from a certain group. (This defaults to 1 for each group. Set to 0 to remove the limit.)\n\n`>🎨rename <group name> <new group name>`\nRename a role group.\n\n`>🎨delete <group name>`\nDelete a role group.";
+		help += "\n\nWith role management permission, you can use the following commands.\n\n`>🎨open`\nToggle open color mode. This is disabled by default.\n\n`>🎨create <group name>`\nCreate a role group.\n\n`>🎨group <group name> <role name>`\nAdd a role to a role group.\n\n`>🎨ungroup <role name>`\nRemove a role from its role group.\n\n`>🎨limit <group name> <number>`\nLimit how many roles each user can have from a certain group. (This defaults to 1 for each group. Set to 0 to remove the limit.)\n\n`>🎨rename <group name> <new group name>`\nRename a role group.\n\n`>🎨delete <group name>`\nDelete a role group.";
 	}
 	help += "\n\nTo invite me to one of your own Discord servers, you can go to <https://miroware.io/discord/colorbot/>.";
 	msg.channel.send(help).catch(errSendMessages(msg));
@@ -167,7 +167,7 @@ client.on("message", async msg => {
 		let content = msg.content;
 		if (prefix.test(content)) {
 			const member = msg.guild.member(msg.author) || await msg.guild.members.fetch(msg.author);
-			const perm = member.hasPermission(8) || member.id === "152282430915608578";
+			const perm = member.hasPermission(268435456) /* Manage Roles */ || member.id === "152282430915608578";
 			content = content.replace(prefix, "");
 			if (content) {
 				content = content.replace(spaces, " ");
@@ -181,7 +181,7 @@ client.on("message", async msg => {
 				const contentIsColor = content[0] === "color" || content[0] === "colour";
 				if (contentIsColor || content[0] === "reset") {
 					if (data.guilds[msg.guild.id][0] === 0) {
-						msg.channel.send(`${msg.author} This command is unavailable, as open color mode is disabled.${perm ? ' With administrative permission, you can enable it by entering ">🎨open".' : ""}`).catch(errSendMessages(msg));
+						msg.channel.send(`${msg.author} This command is unavailable, as open color mode is disabled.${perm ? ' With role management permission, you can enable it by entering ">🎨open".' : ""}`).catch(errSendMessages(msg));
 					} else if (contentIsColor) {
 						if (content[1]) {
 							if (colorTest.test(content[1])) {
