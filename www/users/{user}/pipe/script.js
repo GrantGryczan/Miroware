c680091c07bd454aab5163f8e6e096638953ce28
Grant Gryczan <grantgryczan@gmail.com>
2020-05-30 19:20:48 -0400

    Syntax error

--- www/users/{user}/pipe/script.js
+++ www/users/{user}/pipe/script.js
@@ -1257,7 +1257,7 @@ if (Miro.data.isMe) {
 			if (file) {
 				let name = (file = file.getAsFile()).name;
 				if (htmlString) {
-					const htmlFilename = (await new Promise(htmlString.getAsString.bind(htmlString)).match(htmlFilenameTest);
+					const htmlFilename = (await new Promise(htmlString.getAsString.bind(htmlString))).match(htmlFilenameTest);
 					name = htmlFilename ? htmlFilename[1] : "file";
 				}
 				addFile(file, undefined, await enterFileName("Paste", name));
