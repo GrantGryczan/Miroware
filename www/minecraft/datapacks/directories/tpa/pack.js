9a51baf2d97000abf91b28ebd9db3e831e5f5ee6
Grant Gryczan <grantgryczan@gmail.com>
2020-08-01 07:44:05 -0400

    Remove TPA config description

--- www/minecraft/datapacks/directories/tpa/pack.js
+++ www/minecraft/datapacks/directories/tpa/pack.js
@@ -6,9 +6,6 @@ module.exports = {
 	description: `
 		Enter "/trigger tpa set &lt;PID&gt;" to request to teleport to another player.<br>
 		Each player's PID (player ID) number can be found in the player list by pressing Tab by default.<br>
-		<span class="more">
-			Enter "/function tpa:config" to configure the time players must stand still before teleportation after their teleport request is accepted.
-		</span><br>
 		<b>Incompatible with Death Counter</b>
 	`,
 	tags: []
