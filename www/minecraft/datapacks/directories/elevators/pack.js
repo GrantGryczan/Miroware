2ef0b4cdce79c50b8d64a3e4257dbeea3cf44faa
Grant Gryczan <grantgryczan@gmail.com>
2020-08-06 05:23:41 -0400

    Update Elevators description

--- www/minecraft/datapacks/directories/elevators/pack.js
+++ www/minecraft/datapacks/directories/elevators/pack.js
@@ -5,8 +5,8 @@ module.exports = {
 	compatibility: ["1.16"],
 	description: `
 		Drop an ender pearl onto a wool block to make it into an elevator.<br>
-		Jump on it to warp onto an elevator directly above.<br>
-		Crouch on it to warp onto an elevator directly below.
+		Jump on it to warp onto an elevator of the same color directly above.<br>
+		Crouch on it to warp onto an elevator of the same color directly below.
 	`,
 	tags: []
 };
