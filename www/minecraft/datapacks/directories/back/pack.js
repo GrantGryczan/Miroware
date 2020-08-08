d784dfef5b9339865d159dbe9e4cb6f2687fa16a
Grant Gryczan <grantgryczan@gmail.com>
2020-08-08 04:48:03 -0400

    Remove Back dependencies

--- www/minecraft/datapacks/directories/back/pack.js
+++ www/minecraft/datapacks/directories/back/pack.js
@@ -7,8 +7,7 @@ module.exports = {
 		Enter "/trigger back" to go back to the last location you teleported from, or your last death location if that's enabled.<br>
 		<span class="more">
 			Enter "/function back:config" to configure the time players must stand still before teleporting after running the back command or whether to save death locations in the back command.
-		</span><br>
-		<b>Requires Homes, Spawn, or TPA</b>
+		</span>
 	`,
 	tags: []
 };
