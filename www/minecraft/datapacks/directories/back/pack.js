8572295424553bcf2b8883666b34dae06e0c3b7a
Grant Gryczan <grantgryczan@gmail.com>
2020-08-13 16:36:47 -0400

    Update Back description

--- www/minecraft/datapacks/directories/back/pack.js
+++ www/minecraft/datapacks/directories/back/pack.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "Back",
 	compatibility: ["1.16"],
 	description: `
-		Enter "/trigger back" to go back to the last location you teleported from (using Homes, Spawn, or TPA), or to your last death location if that's enabled.<br>
+		Enter "/trigger back" to go back to the last location you teleported from (using Homes, Spawn, TPA, or Back), or to your last death location if that's enabled.<br>
 		<span class="more">
 			Enter "/function back:config" to configure the time players must stand still before teleporting after running the back command or whether to save death locations in the back command.
 		</span>
