0c2ac7cb2c8acc90512e8a49cbdbfb4fe0ccd251
Grant Gryczan <grantgryczan@gmail.com>
2020-08-17 04:36:40 -0400

    Fix MP Sleep description

--- www/minecraft/datapacks/directories/multiplayer_sleep_2/pack.js
+++ www/minecraft/datapacks/directories/multiplayer_sleep_2/pack.js
@@ -4,7 +4,7 @@ module.exports = {
 	name: "Multiplayer Sleep",
 	compatibility: ["1.16"],
 	description: `
-		Only a fraction of players in the overworld must sleep to skip the night and the rain.<br>
+		Not all players in the overworld must sleep to skip the night and the rain.<br>
 		<span class="more">
 			Enter "/trigger mpSleep" for details and settings.<br>
 			Enter "/function multiplayer_sleep:config" to configure the percent of players required to sleep or the color of the sleep progress boss bar.
