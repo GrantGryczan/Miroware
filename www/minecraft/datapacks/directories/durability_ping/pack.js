d23937658f861e0742e907ca227411449b875f2a
Grant Gryczan <grantgryczan@gmail.com>
2020-08-21 02:20:16 -0400

    Fix Durability Ping metadata

--- www/minecraft/datapacks/directories/durability_ping/pack.js
+++ www/minecraft/datapacks/directories/durability_ping/pack.js
@@ -4,7 +4,11 @@ module.exports = {
 	name: "Durability Ping",
 	compatibility: ["1.16"],
 	description: `
-		
+		Get notified when you damage an item with 10% or less durability.<br>
+		<span class="more">
+			Enter "/trigger duraPing" for personal settings.<br>
+			Enter "/function durability_ping:config" for config options.
+		</span>
 	`,
 	tags: []
 };
--- www/minecraft/datapacks/directories/multiplayer_sleep/pack.js
+++ www/minecraft/datapacks/directories/multiplayer_sleep/pack.js
@@ -6,7 +6,7 @@ module.exports = {
 	description: `
 		Not all players in the overworld must sleep to skip the night and the rain.<br>
 		<span class="more">
-			Enter "/trigger mpSleep" for details and settings.<br>
+			Enter "/trigger mpSleep" for details and personal settings.<br>
 			Enter "/function multiplayer_sleep:config" to configure the percent of players required to sleep or the color of the sleep progress boss bar.
 		</span>
 	`,
