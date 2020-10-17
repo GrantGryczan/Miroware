9527af9262635987b8e97b22c79f361b84efc6b7
Grant Gryczan <grantgryczan@gmail.com>
2020-10-17 19:27:40 -0400

    No Dragon Griefing

---
+++ www/minecraft/datapacks/directories/no_dragon_griefing/pack.js
@@ -0,0 +1,10 @@
+module.exports = {
+	namespace: "no_dragon_griefing",
+	version: "1.0.0",
+	name: "No Dragon Griefing",
+	compatibility: ["1.16"],
+	description: `
+		Disable dragons breaking blocks.
+	`,
+	tags: []
+};
