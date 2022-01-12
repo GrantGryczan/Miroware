540dffcec51da3158f3c0cbebc4f8c8d06a90823
Grant Gryczan <grantgryczan@gmail.com>
2022-01-11 20:42:47 -0500

    Delete Mirobot from startup

--- startup.sh
+++ startup.sh
@@ -4,6 +4,5 @@ screen -d -m ./forever.sh ./proxy.js
 screen -d -m ./forever.sh ./server.js production
 screen -d -m ./forever.sh ./pipe.js
 screen -d -m ./forever.sh ./concat.js
-screen -d -m ./forever.sh ./mirobot.js
 screen -d -m ./forever.sh ./starbot.js
 screen -d -m ./forever.sh ./colorbot.js
