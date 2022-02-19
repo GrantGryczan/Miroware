3befd6dab03791a84995279a642c7f72b1f2c7b1
Grant Gryczan <grantgryczan@gmail.com>
2022-02-19 06:44:49 -0500

    Update links on input rather than on button click

--- www/pipe/link-updater/index.njs
+++ www/pipe/link-updater/index.njs
@@ -15,7 +15,6 @@ this.value += html`
 					<textarea id="input" class="mdc-text-field__input" rows="8"></textarea>
 					<label class="mdc-floating-label" for="input">Input</label>
 				</div>
-				<button id="convert" class="mdc-button mdc-button--raised mdc-ripple">Convert</button><br>
 				<div class="mdc-text-field mdc-text-field--textarea mdc-text-field--fullwidth">
 					<textarea id="output" class="mdc-text-field__input" rows="8" readonly></textarea>
 					<label class="mdc-floating-label alwaysFloat" for="output">Output</label>
--- www/pipe/link-updater/script.js
+++ www/pipe/link-updater/script.js
@@ -2,6 +2,9 @@
 const input = document.body.querySelector("#input");
 const output = document.body.querySelector("#output");
 output.addEventListener("focus", output.select.bind(output));
-document.body.querySelector("#convert").addEventListener("click", () => {
+const update = () => {
 	output.value = input.value.replace(/pipe\.miroware\.io\/([0-9a-f]{24})/g, (match, hex) => `file.garden/${btoa(hex.match(/\w{2}/g).map(byte => String.fromCharCode(parseInt(byte, 16))).join("")).replace(/\//g, "-").replace(/\+/g, "_")}`);
-});
+};
+input.addEventListener("input", update);
+input.addEventListener("keydown", update);
+input.addEventListener("change", update);
