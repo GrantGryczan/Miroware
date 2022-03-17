eca913b40136e883fff8a56a372bd72894b9f41a
Grant Gryczan <grantgryczan@gmail.com>
2022-03-17 04:38:00 -0400

    Improve `enableMaintenance`

--- proxy.js
+++ proxy.js
@@ -26,7 +26,7 @@ const proxy = httpProxy.createProxyServer();
 let maintenance;
 const enableMaintenance = () => {
 	maintenance = Math.random();
-	return `document.cookie="temp=${maintenance};domain=.miroware.io;path=/;expires="+new Date(Date.now()+1000*60*60*24*7).toUTCString();`;
+	console.log(`document.cookie="temp=${maintenance};domain=.miroware.io;path=/;expires="+new Date(Date.now()+1000*60*60*24*7).toUTCString();`);
 };
 const disableMaintenance = () => {
 	maintenance = '';
