b3bbbca0e793b7281ef58dfa71831c9809fb3416
Grant Gryczan <grantgryczan@gmail.com>
2026-08-26 07:25:37 -0400

    Fix file server indexes

--- startup.sh
+++ startup.sh
@@ -7,8 +7,8 @@ tmux new -d -s server2 ./forever.sh ./server.js 2
 tmux new -d -s server3 ./forever.sh ./server.js 3
 tmux new -d -s pipe0 ./forever.sh ./pipe.js 0
 tmux new -d -s pipe1 ./forever.sh ./pipe.js 1
-tmux new -d -s pipe2 ./forever.sh ./pipe.js 3
-tmux new -d -s pipe3 ./forever.sh ./pipe.js 4
+tmux new -d -s pipe2 ./forever.sh ./pipe.js 2
+tmux new -d -s pipe3 ./forever.sh ./pipe.js 3
 tmux new -d -s concat ./forever.sh ./concat.js
 tmux new -d -s starbot ./forever.sh ./starbot.js
 tmux new -d -s colorbot ./forever.sh ./colorbot.js
